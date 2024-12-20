b26a3c8fe62c27f63a79a407f2fb2763d8d2989c
Livai04 <muzusifi@gmail.com>
2024-12-20 10:32:41 +0100

    ajout d'un  pour le formulaire
    formulaire n'ayant pas de container  il prenait toute la page, ajout d'un container bootstraap.

--- formulaire.php
+++ formulaire.php
@@ -50,7 +50,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 }
 ?>
     
-<!DOCTYPE html>
+    <!DOCTYPE html>
 <html lang="fr">
 
 <head>
@@ -84,81 +84,83 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             </div>
         </div>
     </div>
-    <!-- Formulaire -->
-    <form class="form-section">
-        <div class="mb-4">
-            <label class="form-label">Civilité :</label>
-            <div>
-                <input type="radio" name="civilite" id="madame" value="Madame" checked>
-                <label for="madame" class="ms-2">Madame</label>
-                <input type="radio" name="civilite" id="monsieur" value="Monsieur" class="ms-3">
-                <label for="monsieur" class="ms-2">Monsieur</label>
-                <input type="radio" name="civilite" id="autre" value="Autre" class="ms-3">
-                <label for="autre" class="ms-2">Autre</label>
-            </div>
-        </div>
 
-        <div class="row g-3">
-            <div class="col-md-6">
-                <label for="nom" class="form-label">Nom</label>
-                <input type="text" id="nom" class="custom-input" placeholder="Nom">
-            </div>
-            <div class="col-md-6">
-                <label for="prenom" class="form-label">Prénom</label>
-                <input type="text" id="prenom" class="custom-input" placeholder="Prénom">
-            </div>
-            <div class="col-md-6">
-                <label for="email" class="form-label">Email*</label>
-                <input type="email" id="email" class="custom-input" placeholder="Email">
-            </div>
-            <div class="col-md-6">
-                <label for="telephone" class="form-label">Téléphone</label>
-                <input type="text" id="telephone" class="custom-input" placeholder="Téléphone">
-            </div>
-        </div>
-
-        <div class="row g-3 mt-3">
-            <div class="col-md-6">
-                <label for="adresse" class="form-label">Adresse</label>
-                <input type="text" id="adresse" class="custom-input" placeholder="Adresse">
-            </div>
-            <div class="col-md-6">
-                <label for="ville" class="form-label">Ville</label>
-                <input type="text" id="ville" class="custom-input" placeholder="Ville">
-            </div>
-            <div class="col-md-6">
-                <label for="departement" class="form-label">Département</label>
-                <input type="text" id="departement" class="custom-input" placeholder="Département">
-            </div>
-            <div class="col-md-6">
-                <label for="codepostal" class="form-label">Code Postal</label>
-                <input type="text" id="codepostal" class="custom-input" placeholder="Code Postal">
-            </div>
-        </div>
-
-        <div class="row g-3 mt-4">
-            <!-- Champ Niveau -->
-            <div class="col-md-6">
-                <label for="niveau" class="form-label">Niveau</label>
-                <select id="niveau" class="custom-input">
-                    <option selected disabled>Choisissez votre niveau de plongée</option>
-                    <option value="debutant">Débutant</option>
-                    <option value="intermediaire">Intermédiaire</option>
-                    <option value="avance">Avancé</option>
-                </select>
+    <!-- Formulaire dans un conteneur -->
+    <div class="container my-5">
+        <form class="form-section">
+            <div class="mb-4">
+                <label class="form-label">Civilité :</label>
+                <div>
+                    <input type="radio" name="civilite" id="madame" value="Madame" checked>
+                    <label for="madame" class="ms-2">Madame</label>
+                    <input type="radio" name="civilite" id="monsieur" value="Monsieur" class="ms-3">
+                    <label for="monsieur" class="ms-2">Monsieur</label>
+                    <input type="radio" name="civilite" id="autre" value="Autre" class="ms-3">
+                    <label for="autre" class="ms-2">Autre</label>
+                </div>
             </div>
 
-            <!-- Champ Commentaires -->
-            <div class="col-md-6">
-                <label for="commentaires" class="form-label">Commentaires Supplémentaires</label>
-                <textarea id="commentaires" rows="3" class="custom-input" placeholder="Votre commentaire"></textarea>
+            <div class="row g-3">
+                <div class="col-md-6">
+                    <label for="nom" class="form-label">Nom</label>
+                    <input type="text" id="nom" class="custom-input form-control" placeholder="Nom">
+                </div>
+                <div class="col-md-6">
+                    <label for="prenom" class="form-label">Prénom</label>
+                    <input type="text" id="prenom" class="custom-input form-control" placeholder="Prénom">
+                </div>
+                <div class="col-md-6">
+                    <label for="email" class="form-label">Email*</label>
+                    <input type="email" id="email" class="custom-input form-control" placeholder="Email">
+                </div>
+                <div class="col-md-6">
+                    <label for="telephone" class="form-label">Téléphone</label>
+                    <input type="text" id="telephone" class="custom-input form-control" placeholder="Téléphone">
+                </div>
             </div>
-        </div>
 
-        <div class="button-container mt-4">
-            <button type="submit" class="btn btn-primary">Terminer</button>
-        </div>
-    </form>
+            <div class="row g-3 mt-3">
+                <div class="col-md-6">
+                    <label for="adresse" class="form-label">Adresse</label>
+                    <input type="text" id="adresse" class="custom-input form-control" placeholder="Adresse">
+                </div>
+                <div class="col-md-6">
+                    <label for="ville" class="form-label">Ville</label>
+                    <input type="text" id="ville" class="custom-input form-control" placeholder="Ville">
+                </div>
+                <div class="col-md-6">
+                    <label for="departement" class="form-label">Département</label>
+                    <input type="text" id="departement" class="custom-input form-control" placeholder="Département">
+                </div>
+                <div class="col-md-6">
+                    <label for="codepostal" class="form-label">Code Postal</label>
+                    <input type="text" id="codepostal" class="custom-input form-control" placeholder="Code Postal">
+                </div>
+            </div>
+
+            <div class="row g-3 mt-4">
+                <!-- Champ Niveau -->
+                <div class="col-md-6">
+                    <label for="niveau" class="form-label">Niveau</label>
+                    <select id="niveau" class="custom-input form-control">
+                        <option selected disabled>Choisissez votre niveau de plongée</option>
+                        <option value="debutant">Débutant</option>
+                        <option value="intermediaire">Intermédiaire</option>
+                        <option value="avance">Avancé</option>
+                    </select>
+                </div>
+
+                <!-- Champ Commentaires -->
+                <div class="col-md-6">
+                    <label for="commentaires" class="form-label">Commentaires Supplémentaires</label>
+                    <textarea id="commentaires" rows="3" class="custom-input form-control" placeholder="Votre commentaire"></textarea>
+                </div>
+            </div>
+
+            <div class="button-container mt-4">
+                <button type="submit" class="btn btn-primary">Terminer</button>
+            </div>
+        </form>
     </div>
 
     <?php include 'footer.php'; ?>
